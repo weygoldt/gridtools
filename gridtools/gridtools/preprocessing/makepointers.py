a059c75b07127cd0c589c23a5732b99b207b947d
weygoldt <88969563+weygoldt@users.noreply.github.com>
2022-12-20 16:24:24 +0100

    iterative pointers implemented but not testetd yet

--- gridtools/gridtools/preprocessing/makepointers.py
+++ gridtools/gridtools/preprocessing/makepointers.py
@@ -6,7 +6,6 @@ from IPython import embed
 
 from gridtools.utils.datahandling import estimateMode
 
-
 class NixGridRecording:
     """
     Loads a single recording from a .nix file containing multiple recordings.
@@ -123,6 +122,11 @@ class ConnectFish:
         )  # window to compute baseline for pointing
 
         # Get start and end points of all recordings
+        pointers = [] # the newly generated pointers go here
+        previous_pointers = [] # save previous pointers in this buffer
+        track_ids = [] # the old track ids go here
+        previous_matches_rec2 = [] # save previous pairs in this buffer
+        rec_names = [] # the recording names go here
         for rec1, rec2 in zip(self.recordings[:-1], self.recordings[1:]):
 
             # get time distance between recordings
@@ -201,7 +205,10 @@ class ConnectFish:
             matches_rec2 = ids2[idxs1]
 
             # create new meta ids
-            meta_ids = np.arange(len(matches_rec1))
+            lastmax = 0
+            if len(previous_pointers) != 0:
+                lastmax = np.max(previous_pointers) 
+            pointers_tmp = np.arange(lastmax + 1 , len(matches_rec1) + 1)
 
             # reshape matching ids
             matches = [[x, y] for x, y in zip(matches_rec1, matches_rec2)]
@@ -209,9 +216,39 @@ class ConnectFish:
             # make recording references
             recording_ref = [[rec1.name, rec2.name] for i in range(len(matches))]
 
+            # check if track ids from currently first recording are already referenced in the previously last recording
+            
+            # step 1: get ids in current rec1 that where already in previous rec2
+            repeated_matches = [x for x in matches_rec1 if x in previous_matches_rec2] # get repeated
+            
+            if len(repeated_matches) != 0:
+
+                # step 2: find pointers of these cases
+                indices = np.arange(len(previous_matches_rec2))
+                rep_indices = np.asarray([indices[previous_matches_rec2 == x] for x in repeated_matches])
+                print(rep_indices)
+                rep_pointers = np.asarray(previous_pointers)[rep_indices]
+
+                # step 3: swap the newly created pointers for the previous pointers
+                indices = np.arange(len(matches_rec1))
+                rep_indices = np.asarray([indices[matches_rec1 == x] for x in repeated_matches])
+                np.asarray(pointers_tmp)[rep_indices] = rep_pointers
+
+            # step 4: append all to lists
+            pointers.extend(pointers_tmp)
+            track_ids.extend(matches)
+            rec_names.extend(recording_ref)
+            
+            # step 5: save data from current iteration to buffer to compare in nex iteration
+            previous_poiners = pointers_tmp 
+            previous_matches_rec2 = matches_rec2
+
             if len(recording_ref) != 0:
                 embed()
 
+        print(pointers)
+        print(track_ids)
+        print(rec_names)
 
 if __name__ == "__main__":
 
