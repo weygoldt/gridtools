1ac10fa69e8170c7c64b04c4d9a37d323d0bd6ab
weygoldt <88969563+weygoldt@users.noreply.github.com>
2023-03-09 17:03:11 +0100

    add fish extraction experiment

--- fakegrid/example.py
+++ fakegrid/example.py
@@ -5,6 +5,7 @@ if __name__ != '__main__':
 import numpy as np
 from numpy.fft import rfft
 
+from IPython import embed
 from ssqueezepy import ssq_cwt, issq_cwt, cwt
 from ssqueezepy.toolkit import lin_band, cos_f, mad_rms
 from ssqueezepy.visuals import imshow, plot, scat
@@ -19,12 +20,12 @@ N = 2048
 noise_var = 6  # noise variance; compare error against = 12
 
 x, ts = echirp(N)
+
 x *= (1 + .3 * cos_f([1], N))  # amplitude modulation
 xo = x.copy()
 np.random.seed(4)
 x += np.sqrt(noise_var) * np.random.randn(len(x))
 
-from IPython import embed
 
 #### Show signal & its global spectrum #######################################
 axf = np.abs(rfft(x))
@@ -47,6 +48,8 @@ imshow(_Tx, norm=(0, 4e-1), **pkw)
 #%%# Estimate inversion ridge ###############################################
 bw, slope, offset = .035, -.46, -1.8
 Cs, freqband = lin_band(Tx, slope, offset, bw, norm=(0, 4e-1))
+
+embed()
 
 #%%###########################################################################
 xrec = issq_cwt(Tx, kw['wavelet'], Cs, freqband)[0]
